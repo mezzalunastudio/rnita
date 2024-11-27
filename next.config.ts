4bd1f5828be51c5d992ef74136bcf5df0dacecc1
Alif Laksono <alif7laksono@gmail.com>
2024-11-27 20:37:38 +0700

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/wedding-and-makeup",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
