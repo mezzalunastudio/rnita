f0c86fa0cecd05011a09b621edf1a2330cb97e05
Alif Laksono <alif7laksono@gmail.com>
2024-12-02 13:45:53 +0700

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/wedding-and-makeup",
-  //       // permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/wedding-and-makeup",
+        // permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
